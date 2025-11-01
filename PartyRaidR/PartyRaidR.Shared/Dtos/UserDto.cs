171eca9ec3fc672998faaa5f797eddb4037a5b3a
Tóth Máté <t0thm4t3@gmail.com>
2025-11-01 14:28:59 +0100

    Removed unnecessary information from DTO class

--- PartyRaidR/PartyRaidR.Shared/Dtos/UserDto.cs
+++ PartyRaidR/PartyRaidR.Shared/Dtos/UserDto.cs
@@ -7,7 +7,6 @@ namespace PartyRaidR.Shared.Dtos
         public string Id { get; set; } = Guid.Empty.ToString();
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
         public string ProfilePictureUrl { get; set; } = string.Empty;
         public DateTime RegisterDate { get; set; } = DateTime.Now;
         public UserRole Role { get; set; } = UserRole.User;
