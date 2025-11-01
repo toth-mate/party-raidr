89a474b1520fefbd321be88873a8b17096cdbfc2
Tóth Máté <t0thm4t3@gmail.com>
2025-11-01 13:58:09 +0100

    Response classes

---
+++ PartyRaidR/PartyRaidR.Shared/Dtos/CityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PartyRaidR.Shared.Dtos
+{
+    public class CityDto
+    {
+        public string Id { get; set; } = Guid.Empty.ToString();
+        public string Name { get; set; } = string.Empty;
+        public int ZipCode { get; set; } = 0;
+        public string County { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
---
+++ PartyRaidR/PartyRaidR.Shared/Dtos/NotificationDto.cs
@@ -0,0 +1,16 @@
+﻿using PartyRaidR.Shared.Models;
+
+namespace PartyRaidR.Shared.Dtos
+{
+    public class NotificationDto
+    {
+        public string Id { get; set; } = Guid.Empty.ToString();
+        public string Title { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string EventId { get; set; } = string.Empty;
+        public NotificationType Type { get; set; } = NotificationType.General;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsRead { get; set; } = false;
+    }
+}
