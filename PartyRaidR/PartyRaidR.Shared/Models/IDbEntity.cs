62860a5652af4cdc943deeb0fc2563fbdb02bfa2
Tóth Máté <t0thm4t3@gmail.com>
2025-11-01 14:01:15 +0100

    Fixed HasId condition for databse models

--- PartyRaidR/PartyRaidR.Shared/Models/IDbEntity.cs
+++ PartyRaidR/PartyRaidR.Shared/Models/IDbEntity.cs
@@ -2,8 +2,8 @@
 {
     public interface IDbEntity<TEntity> where TEntity : class, new()
     {
-        string Id { get;set; }
-        bool HasId => Id is not null && Id != string.Empty;
+        string Id { get; set; }
+        bool HasId => Id is not null && Id != string.Empty && !Id.All(i => i == '0');
         string DbSetName => string.Concat(new TEntity().GetType(), 's');
     }
 }
