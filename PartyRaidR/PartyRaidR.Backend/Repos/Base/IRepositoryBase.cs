931e3ff04c496126c8d141a6f6158d3c2ceec414
Tóth Máté <t0thm4t3@gmail.com>
2025-11-09 21:19:43 +0100

    CRUD operations (Base Repository)

---
+++ PartyRaidR/PartyRaidR.Backend/Repos/Base/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using PartyRaidR.Shared.Models;
+
+namespace PartyRaidR.Backend.Repos.Base
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class, IDbEntity<TEntity>, new()
+    {
+        Task<TEntity> GetByIdAsync(string id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task InsertAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ PartyRaidR/PartyRaidR.Backend/Repos/Base/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using PartyRaidR.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PartyRaidR.Backend.Repos.Base
+{
+    public class RepositoryBase<TDbContext, TEntity> : IRepositoryBase<TEntity>
+        where TDbContext : DbContext
+        where TEntity : class, IDbEntity<TEntity>, new()
+    {
+        private readonly TDbContext _context;
+        protected readonly DbSet<TEntity>? _dbSet;
+
+        public RepositoryBase(TDbContext? context)
+        {
+            ArgumentNullException.ThrowIfNull(context, $"Table '{nameof(context)}' cannot be accessed.");
+            _context = context;
+            _dbSet = _context.Set<TEntity>() ?? throw new ArgumentException($"Table '{nameof(TEntity)}' cannot be accessed.");
+        }
+
+        // CRUD Operations
+        // An empty entity is returned if no entity is found with the given ID.
+        public async Task<TEntity> GetByIdAsync(string id) =>
+            await _dbSet!.FindAsync(id) ?? new TEntity();
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() =>
+            await _dbSet!.ToListAsync();
+
+        public async Task InsertAsync(TEntity entity) =>
+            await _dbSet!.AddAsync(entity);
+
+        public void Update(TEntity entity) =>
+            _dbSet!.Update(entity);
+
+        public void Delete(TEntity entity) =>
+            _dbSet!.Remove(entity);
+
+        public async Task<int> SaveChangesAsync() =>
+            await _context.SaveChangesAsync();
+    }
+}
