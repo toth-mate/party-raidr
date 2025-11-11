c43962628ca48789b6be0bf435de72ca7ee1fc85
Tóth Máté <t0thm4t3@gmail.com>
2025-11-11 16:33:29 +0100

    Repository classes for database operations

---
+++ PartyRaidR/PartyRaidR.Backend/Repos/NotificationRepo.cs
@@ -0,0 +1,14 @@
+﻿using PartyRaidR.Backend.Context;
+using PartyRaidR.Backend.Repos.Base;
+using PartyRaidR.Backend.Repos.Promises;
+using PartyRaidR.Shared.Models;
+
+namespace PartyRaidR.Backend.Repos
+{
+    public class NotificationRepo : RepositoryBase<Notification>, INotificationRepo
+    {
+        public NotificationRepo(AppDbContext? context) : base(context)
+        {
+        }
+    }
+}
