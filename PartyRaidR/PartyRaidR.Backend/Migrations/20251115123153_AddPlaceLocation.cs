736c6c039437e4865d82167c79c0f95f30852823
Tóth Máté <t0thm4t3@gmail.com>
2025-11-15 13:37:13 +0100

    Updated database schema

---
+++ PartyRaidR/PartyRaidR.Backend/Migrations/20251115123153_AddPlaceLocation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace PartyRaidR.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlaceLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GpsLattitude",
+                table: "Places");
+
+            migrationBuilder.DropColumn(
+                name: "GpsLongitude",
+                table: "Places");
+
+            migrationBuilder.AddColumn<Point>(
+                name: "Location",
+                table: "Places",
+                type: "point",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Places");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GpsLattitude",
+                table: "Places",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GpsLongitude",
+                table: "Places",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
