8905bceb029672415e0f3e0727f42000064430a1
Tóth Máté <t0thm4t3@gmail.com>
2025-11-28 14:11:17 +0100

    Authentication service

---
+++ src/PartyRaidR.Backend/Services/AuthenticationService.cs
@@ -0,0 +1,32 @@
+﻿using PartyRaidR.Backend.Repos;
+using PartyRaidR.Backend.Repos.Promises;
+using PartyRaidR.Backend.Services.Promises;
+using PartyRaidR.Shared.Assemblers;
+using PartyRaidR.Shared.Dtos;
+using PartyRaidR.Shared.Dtos.AuthenticationRequests;
+using PartyRaidR.Shared.Models.Responses;
+
+namespace PartyRaidR.Backend.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IUserRepo _userRepo;
+        private readonly UserAssembler _userAssembler;
+
+        public AuthenticationService(IUserRepo userRepo, UserAssembler userAssembler)
+        {
+            _userRepo = userRepo;
+            _userAssembler = userAssembler;
+        }
+
+        public Task<ServiceResponse<string>> LoginAsync(UserLoginDto user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<UserDto>> RegisterAsync(UserRegistrationDto user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
