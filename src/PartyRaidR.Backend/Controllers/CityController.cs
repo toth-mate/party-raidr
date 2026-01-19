f4f0e69477af92b0a601a5001c9a00e40cd54c15
Tóth Máté <t0thm4t3@gmail.com>
2026-01-19 22:32:23 +0100

    City - API endpoints

---
+++ src/PartyRaidR.Backend/Controllers/CityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyRaidR.Backend.Services.Base;
+using PartyRaidR.Backend.Services.Promises;
+using PartyRaidR.Shared.Dtos;
+using PartyRaidR.Shared.Models;
+
+namespace PartyRaidR.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CityController : BaseController<City, CityDto>
+    {
+        public CityController(ICityService service) : base(service)
+        {
+        }
+    }
+}
