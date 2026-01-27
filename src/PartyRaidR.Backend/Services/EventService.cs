e3b54a51d9fd110d0946735919cbdfb8810fe5d8
Tóth Máté <t0thm4t3@gmail.com>
2026-01-27 15:16:36 +0100

    Creating an event - conditions

--- src/PartyRaidR.Backend/Services/EventService.cs
+++ src/PartyRaidR.Backend/Services/EventService.cs
@@ -4,6 +4,7 @@ using PartyRaidR.Backend.Services.Promises;
 using PartyRaidR.Shared.Assemblers;
 using PartyRaidR.Shared.Dtos;
 using PartyRaidR.Shared.Models;
+using PartyRaidR.Shared.Models.Responses;
 
 namespace PartyRaidR.Backend.Services
 {
@@ -11,6 +12,19 @@ namespace PartyRaidR.Backend.Services
     {
         public EventService(EventAssembler assembler, IEventRepo? repo, IUserContext userContext) : base(assembler, repo, userContext)
         {
+        }
+
+        public override async Task<ServiceResponse<EventDto>> AddAsync(EventDto dto)
+        {
+            if(dto.Title == string.Empty)
+                throw new ArgumentException("Title cannot be empty.");
+
+            if(dto.Description == string.Empty)
+                throw new ArgumentException("Description cannot be empty.");
+
+            dto.AuthorId = _userContext.UserId;
+
+            return await base.AddAsync(dto);
         }
     }
 }
