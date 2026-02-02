e6c12cab3a0a6b2695c7cdbecfe31c1bec1b646a
Tóth Máté <t0thm4t3@gmail.com>
2026-02-02 22:35:10 +0100

    Filter object for event filtering

---
+++ src/PartyRaidR.Shared/Dtos/EventFilterDto.cs
@@ -0,0 +1,17 @@
+﻿using PartyRaidR.Shared.Models;
+
+namespace PartyRaidR.Shared.Dtos
+{
+    public class EventFilterDto
+    {
+        public string? Title { get; set; } = null;
+        public string? Description { get; set; } = null;
+        public DateTime? StartingDate { get; set; } = null;
+        public DateTime? EndingDate { get; set; } = null;
+        public string? PlaceId { get; set; } = null;
+        public string? CityId { get; set; } = null;
+        public EventCategory Category { get; set; } = EventCategory.None;
+        public decimal? TicketPriceMin { get; set; } = null;
+        public decimal? TicketPriceMax { get; set; } = null;
+    }
+}
