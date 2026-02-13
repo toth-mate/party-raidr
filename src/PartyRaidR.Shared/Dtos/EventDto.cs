b3adcf122245e25c5c42b56777ac2cb43992917e
Tóth Máté <t0thm4t3@gmail.com>
2026-02-13 11:39:17 +0100

    Events now show live status

--- src/PartyRaidR.Shared/Dtos/EventDto.cs
+++ src/PartyRaidR.Shared/Dtos/EventDto.cs
@@ -16,5 +16,23 @@ namespace PartyRaidR.Shared.Dtos
         public decimal TicketPrice { get; set; } = 0;
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public bool IsActive { get; set; } = true;
+        public EventStatus EventStatus
+        {
+            get
+            {
+                var now = DateTime.UtcNow;
+
+                if (EndingDate < now)
+                    return EventStatus.Past;
+
+                if (StartingDate <= now && EndingDate >= now)
+                    return EventStatus.Live;
+
+                if (StartingDate > now && StartingDate < now.AddHours(3))
+                    return EventStatus.StartingSoon;
+
+                return EventStatus.Upcoming;
+            }
+        }
     }
 }
---
+++ src/PartyRaidR.Shared/Enums/EventStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyRaidR.Shared.Enums
+{
+    public enum EventStatus
+    {
+        Upcoming = 0,
+        StartingSoon = 1,
+        Live = 2,
+        Past = 3
+    }
+}
