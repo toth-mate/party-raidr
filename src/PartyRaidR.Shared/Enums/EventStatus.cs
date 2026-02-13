87d3dee4d18c18775ace45becb84c96aeb1bc034
Tóth Máté <t0thm4t3@gmail.com>
2026-02-13 11:39:35 +0100

    Refactoring - Removed unnecessary usings

--- src/PartyRaidR.Shared/Enums/EventStatus.cs
+++ src/PartyRaidR.Shared/Enums/EventStatus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PartyRaidR.Shared.Enums
+﻿namespace PartyRaidR.Shared.Enums
 {
     public enum EventStatus
     {
