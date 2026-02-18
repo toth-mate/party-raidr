4d0d3c6837b7760ca2e28cfda12def096db96022
b0totmat <t0thm4t3@gmail.com>
2026-02-18 10:13:43 +0100

    Event details page

--- src/PartyRaidR.Web/src/views/EventDetailsView.vue
+++ src/PartyRaidR.Web/src/views/EventDetailsView.vue
@@ -1,3 +1,22 @@
 <template>
-    <h2>{{ $route.params.id }}</h2>
+    <div class="container w-75 border rounded p-4">
+        <h3>Event {{ $route.params.id }}</h3>
+        <p class="text-body-secondary">Event description...</p>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <p>Starting date: 2024.06.30.</p>
+                    <p>Ending date: 2024.07.01.</p>
+                    <p>Place: Place name</p>
+                    <p>City: City name</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <p>Category: Party</p>
+                    <p>Room: Not limited</p>
+                    <p>Ticket price: Not paid</p>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-success w-100 mt-2">Apply</button>
+    </div>
 </template>
