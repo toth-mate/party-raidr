071a703a4f903742effb5d2c259bf1e06f838493
Tóth Máté <t0thm4t3@gmail.com>
2026-02-27 16:33:14 +0100

    Filtering form design

--- src/PartyRaidR.Web/src/views/HomeView.vue
+++ src/PartyRaidR.Web/src/views/HomeView.vue
@@ -24,12 +24,54 @@
 <template>
   <h1>Party RaidR</h1>
   <section id="search" class="container mb-5">
-    <div class="input-group">
-      <input type="text" class="form-control" placeholder="Search for events...">
-      <button class="btn btn-primary">Search</button>
+    <div class="row">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search for events...">
+        <button class="btn btn-primary">Search</button>
+      </div>
     </div>
-    <button class="btn btn-secondary mt-2" v-if="hideFilter" @click="toggleFilter">More filtering options</button>
-    <button class="btn btn-secondary mt-2" v-if="!hideFilter" @click="toggleFilter">Hide filtering options</button>
+    <div class="row mt-2 bg-body-secondary px-1 py-2" v-if="!hideFilter">
+      <h6>Advanced filtering</h6>
+      <div class="mb-2 d-flex">
+        <div class="form-floating flex-fill">
+          <input type="text" class="form-control" placeholder="Search by event description" id="event-description">
+          <label for="event-description">Search by event description</label>
+        </div>
+        <div class="form-floating flex-fill">
+          <input type="text" class="form-control" placeholder="Search by place name" id="event-place-name">
+          <label for="event-place-name">Search by place name</label>
+        </div>
+      </div>
+      <div class="input-group mb-2">
+        <span class="input-group-text">Start date</span>
+        <input type="date" class="form-control form-control-lg" id="event-start-date">
+        <span class="input-group-text">End date</span>
+        <input type="date" class="form-control form-control-lg" id="event-end-date">
+      </div>
+      <div class="input-group mb-2">
+        <span class="input-group-text">Minimum price</span>
+        <input type="number" class="form-control" id="event-price-min" placeholder="0">
+        <span class="input-group-text">Ft</span>
+      </div>
+      <div class="input-group mb-2">
+        <span class="input-group-text">Maximum price</span>
+        <input type="number" class="form-control" id="event-price-max" placeholder="0">
+        <span class="input-group-text">Ft</span>
+      </div>
+      <div class="form-floating">
+        <select class="form-select" id="event-category">
+          <option value="0">None</option>
+          <option value="1">Outdoors activity</option>
+          <option value="2">Indoors activity</option>
+          <option value="3">Concert</option>
+          <option value="4">Festival</option>
+          <option value="5">Party</option>
+        </select>
+        <label for="event-category">Category</label>
+      </div>
+    </div>
+    <button class="btn btn-dark mt-2" v-if="hideFilter" @click="toggleFilter">More filtering options</button>
+    <button class="btn btn-light mt-2" v-if="!hideFilter" @click="toggleFilter">Hide filtering options</button>
   </section>
   <section id="event-list" class="container">
     <div class="row gy-4">
@@ -57,6 +99,7 @@
     </div>
   </section>
 </template>
+
 <style scoped>
   #status {
     border-radius: 50%;
@@ -66,5 +109,15 @@
   #status div {
     border-radius: 50%;
     padding: 5px
+  }
+
+  #event-description {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0
+  }
+  #event-place-name {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-left: 0
   }
 </style>
