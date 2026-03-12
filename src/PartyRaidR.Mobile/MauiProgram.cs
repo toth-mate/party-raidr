5a57d4548804c8a94d1c0de38179fde536a1a73f
Tóth Máté <t0thm4t3@gmail.com>
2026-03-12 18:26:41 +0100

    Configure MAUI Community Toolkit

--- src/PartyRaidR.Mobile/MauiProgram.cs
+++ src/PartyRaidR.Mobile/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace PartyRaidR.Mobile
 {
@@ -9,6 +10,7 @@ namespace PartyRaidR.Mobile
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
