7a154ad5600b851dce45d372a92958eced37d9d2
b0totmat <t0thm4t3@gmail.com>
2026-03-18 20:06:01 +0100

    Register ViewModels and Views (pages)

--- src/PartyRaidR.Mobile/MauiProgram.cs
+++ src/PartyRaidR.Mobile/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
+using PartyRaidR.Mobile.ViewModels;
+using PartyRaidR.Mobile.Views.Pages;
 
 namespace PartyRaidR.Mobile
 {
@@ -16,6 +18,12 @@ namespace PartyRaidR.Mobile
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            builder.Services.AddTransient<MainVM>();
+            builder.Services.AddTransient<LoginVM>();
+
+            builder.Services.AddTransient<MainPage>();
+            builder.Services.AddTransient<LoginPage>();
 
 #if DEBUG
     		builder.Logging.AddDebug();
