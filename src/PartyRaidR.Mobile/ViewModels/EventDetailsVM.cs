c9bffb489edd12c70ba5022401fdff05b1080701
Tóth Máté <t0thm4t3@gmail.com>
2026-03-24 10:10:30 +0100

    Get navigational properties

--- src/PartyRaidR.Mobile/ViewModels/EventDetailsVM.cs
+++ src/PartyRaidR.Mobile/ViewModels/EventDetailsVM.cs
@@ -1,7 +1,16 @@
-﻿namespace PartyRaidR.Mobile.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PartyRaidR.Shared.Dtos;
+
+namespace PartyRaidR.Mobile.ViewModels
 {
+    [QueryProperty(nameof(Event.Id), "id")]
     public partial class EventDetailsVM : BaseVM
     {
+        [ObservableProperty]
+        private EventDisplayDto _event;
 
+        public EventDetailsVM()
+        {
+        }
     }
 }
--- src/PartyRaidR.Mobile/Views/Pages/EventDetailsPage.xaml.cs
+++ src/PartyRaidR.Mobile/Views/Pages/EventDetailsPage.xaml.cs
@@ -1,9 +1,12 @@
+using PartyRaidR.Mobile.ViewModels;
+
 namespace PartyRaidR.Mobile.Views.Pages;
 
 public partial class EventDetailsPage : ContentView
 {
-	public EventDetailsPage()
+	public EventDetailsPage(EventDetailsVM vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 }
