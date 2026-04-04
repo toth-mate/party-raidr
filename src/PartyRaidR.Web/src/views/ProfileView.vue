226f5aede0f2638fa52a77185d5ee7dfdbffa0ae
Tóth Máté <t0thm4t3@gmail.com>
2026-04-04 23:43:39 +0200

    feat: added list of applications

--- src/PartyRaidR.Web/src/views/ProfileView.vue
+++ src/PartyRaidR.Web/src/views/ProfileView.vue
@@ -14,5 +14,14 @@
 </script>
 <template>
     <h1>Profile</h1>
-    <p class="text-center text-secondary fs-1 mt-3">Hello <span class="fw-semibold">{{ authStore.user.username }}!</span></p> 
+    <p class="text-center text-secondary fs-1 mt-3">Hello <span class="fw-semibold">{{ authStore.user.username }}!</span></p>
+
+    <section>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="a in applications">
+                <h5>{{ a.eventId }}</h5>
+                <p class="text-body-tertiary">You applied at: {{ a.timeOfApplication }}</p>
+            </li>
+        </ul>
+    </section>
 </template>
