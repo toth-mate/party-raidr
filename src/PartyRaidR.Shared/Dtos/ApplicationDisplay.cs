d70fd5b174fe8b008c2b8d889e4b5f688ca77007
Tóth Máté <t0thm4t3@gmail.com>
2026-04-05 10:49:58 +0200

    fix: initialize empty GUID ids

--- src/PartyRaidR.Shared/Dtos/ApplicationDisplay.cs
+++ src/PartyRaidR.Shared/Dtos/ApplicationDisplay.cs
@@ -3,8 +3,8 @@ namespace PartyRaidR.Shared.Dtos
     public class ApplicationDisplay : IHasId
     {
         // Application ID
-        public string Id { get; set; } = string.Empty;
-        public string EventId { get; set; } = string.Empty;
+        public string Id { get; set; } = Guid.Empty.ToString();
+        public string EventId { get; set; } = Guid.Empty.ToString();
         public string Title { get; set; } = string.Empty;
         public DateTime DateOfApplication { get; set; } = DateTime.Now;
         public DateTime StartDate { get; set; } = DateTime.Now;
