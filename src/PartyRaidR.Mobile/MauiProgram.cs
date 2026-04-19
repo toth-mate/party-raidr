0a4bf675135aa3b9a5345ec69bd99394b4a368c1
Tóth Máté <t0thm4t3@gmail.com>
2026-04-19 20:31:19 +0200

    feat: registered authentication Refit client

--- src/PartyRaidR.Mobile/MauiProgram.cs
+++ src/PartyRaidR.Mobile/MauiProgram.cs
@@ -36,6 +36,7 @@ namespace PartyRaidR.Mobile
 
             // API Clients
             builder.Services.AddRefitClient<IEventApi>()
+                .AddRefitClient<IAuthApi>()
                 .ConfigureHttpClient(c => c.BaseAddress = new Uri("http://10.0.2.2:8080/api"));
 
 #if DEBUG
