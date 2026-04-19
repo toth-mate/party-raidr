e8009f8bf6daebe5627b5c70478c4edc63774dec
Tóth Máté <t0thm4t3@gmail.com>
2026-04-19 20:38:09 +0200

    feat: login command

--- src/PartyRaidR.Mobile/ViewModels/LoginVM.cs
+++ src/PartyRaidR.Mobile/ViewModels/LoginVM.cs
@@ -1,7 +1,36 @@
-﻿namespace PartyRaidR.Mobile.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PartyRaidR.Mobile.Api;
+using PartyRaidR.Shared.Dtos.AuthenticationRequests;
+using System.Diagnostics;
+
+namespace PartyRaidR.Mobile.ViewModels
 {
     public partial class LoginVM : BaseVM
     {
+        private readonly IAuthApi _authClient;
 
+        [ObservableProperty]
+        private string _email;
+
+        [ObservableProperty]
+        private string _password;
+
+        public LoginVM(IAuthApi authClient)
+        {
+            _authClient = authClient;
+        }
+
+        [RelayCommand]
+        private async Task Login()
+        {
+            UserLoginDto creds = new UserLoginDto
+            {
+                Email = Email,
+                Password = Password
+            };
+            string result = await _authClient.Login(creds);
+            Debug.WriteLine(result);
+        }
     }
 }
