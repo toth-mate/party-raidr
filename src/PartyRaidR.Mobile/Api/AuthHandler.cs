b6062de4f6b7f2c59f7e2a9506dddb573b34ee28
Tóth Máté <t0thm4t3@gmail.com>
2026-04-19 21:11:23 +0200

    feat: attach token to requests and handle 401 status code

---
+++ src/PartyRaidR.Mobile/Api/AuthHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace PartyRaidR.Mobile.Api
+{
+    public class AuthHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            string? token = await SecureStorage.GetAsync("access_token");
+
+            if (!string.IsNullOrEmpty(token))
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                Debug.WriteLine("FAIL: Unauthorized!!!");
+                await Shell.Current.GoToAsync("/profile");
+            }
+
+            return response;
+        }
+    }
+}
