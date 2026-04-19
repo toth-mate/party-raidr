42bef4c1eb40b312d6e37199e7ce015b66173a0e
Tóth Máté <t0thm4t3@gmail.com>
2026-04-19 20:28:31 +0200

    feat: login API endpoint

---
+++ src/PartyRaidR.Mobile/Api/IAuthApi.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using PartyRaidR.Shared.Dtos;
+using PartyRaidR.Shared.Dtos.AuthenticationRequests;
+
+namespace PartyRaidR.Mobile.Api
+{
+    public interface IAuthApi
+    {
+        [Post("/auth/login")]
+        Task<string> Login([Body] UserLoginDto credentials);
+    }
+}
