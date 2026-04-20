172ec3ab3950f504549ee08c2bcbf02c75716c6b
Tóth Máté <t0thm4t3@gmail.com>
2026-04-20 19:43:15 +0200

    feat: authentication service for easy user data access throughout the app

---
+++ src/PartyRaidR.Mobile/Services/AuthService.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json;
+using PartyRaidR.Shared.Dtos;
+using PartyRaidR.Mobile.Api;
+using System.Diagnostics;
+using PartyRaidR.Shared.Dtos.AuthenticationRequests;
+
+namespace PartyRaidR.Mobile.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IAuthApi _authClient;
+
+        public AuthService(IAuthApi authApi)
+        {
+            _authClient = authApi;
+        }
+
+        public UserDto? User
+        {
+            get
+            {
+                string userJson = Preferences.Default.Get("user", string.Empty);
+
+                if (!string.IsNullOrEmpty(userJson))
+                    return JsonSerializer.Deserialize<UserDto>(userJson);
+
+                return null;
+            }
+        }
+
+        public bool IsLoggedIn => User is not null;
+
+        public async Task Login(string email, string password)
+        {
+            try
+            {
+                UserLoginDto creds = new UserLoginDto
+                {
+                    Email = email,
+                    Password = password
+                };
+
+                string token = await _authClient.Login(creds);
+                Debug.WriteLine(token);
+
+                if(!string.IsNullOrEmpty(token))
+                {
+                    await SecureStorage.SetAsync("access_token", token);
+
+                    UserDto? user = await _authClient.GetMe();
+
+                    // The user info is converted to a string to make storing easier.
+                    if (user is not null)
+                    {
+                        string userJson = JsonSerializer.Serialize(user);
+                        Preferences.Default.Set("user", userJson);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"FAIL: {ex.Message}");
+            }
+        }
+
+        public void Logout() =>
+            Preferences.Default.Remove("user");
+    }
+}
---
+++ src/PartyRaidR.Mobile/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using PartyRaidR.Shared.Dtos;
+
+namespace PartyRaidR.Mobile.Services
+{
+    public interface IAuthService
+    {
+        Task Login(string email, string password);
+        void Logout();
+        UserDto? User { get; }
+        bool IsLoggedIn { get; }
+    }
+}
