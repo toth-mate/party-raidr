f3991fde7c7cc7c9f4d91dbf2892fe0441216db9
Tóth Máté <t0thm4t3@gmail.com>
2026-05-01 17:26:45 +0200

    feat: binding context

--- src/PartyRaidR.Mobile/Views/Pages/CreateEventPage.xaml.cs
+++ src/PartyRaidR.Mobile/Views/Pages/CreateEventPage.xaml.cs
@@ -1,9 +1,12 @@
+using PartyRaidR.Mobile.ViewModels;
+
 namespace PartyRaidR.Mobile.Views.Pages;
 
 public partial class CreateEventPage : ContentPage
 {
-	public CreateEventPage()
+	public CreateEventPage(CreateEventVM vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 }
