328f18f70e7572ed1982217c3164e99d72423319
Tóth Máté <t0thm4t3@gmail.com>
2026-05-01 18:47:12 +0200

    feat: API clients - DI on create page

--- src/PartyRaidR.Mobile/ViewModels/CreateEventVM.cs
+++ src/PartyRaidR.Mobile/ViewModels/CreateEventVM.cs
@@ -1,10 +1,16 @@
-﻿namespace PartyRaidR.Mobile.ViewModels
+﻿using PartyRaidR.Mobile.Api;
+
+namespace PartyRaidR.Mobile.ViewModels
 {
     public partial class CreateEventVM : BaseVM
     {
-        public CreateEventVM()
+        private readonly IEventApi _eventClient;
+        private readonly IPlaceApi _placeClient;
+
+        public CreateEventVM(IEventApi eventClient, IPlaceApi placeClient)
         {
-            
+            _placeClient = placeClient;
+            _eventClient = eventClient;
         }
     }
 }
