9bc09805902bd427ac73b85b930374dea4ee0030
toth-mate <t0thm4t3@gmail.com>
2026-05-09 07:54:18 +0200

    chore: configured toast package

--- src/PartyRaidR.Web/src/main.js
+++ src/PartyRaidR.Web/src/main.js
@@ -7,9 +7,16 @@ import router from './router'
 import './assets/main.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+import Vue3Toastify from 'vue3-toastify'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// Vue3Toastify configuration
+app.use(Vue3Toastify, {
+    autoClose: 3000
+})
 
 app.mount('#app')
