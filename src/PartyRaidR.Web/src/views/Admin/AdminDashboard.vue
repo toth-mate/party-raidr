e746550e0cc4a31cadca3840bbfcc12b4f15f992
toth-mate <t0thm4t3@gmail.com>
2026-05-12 19:34:41 +0200

    feat: responsive layout

--- src/PartyRaidR.Web/src/views/Admin/AdminDashboard.vue
+++ src/PartyRaidR.Web/src/views/Admin/AdminDashboard.vue
@@ -1,30 +1,28 @@
 <template>
     <h1>Dashboard</h1>
 
-    <section>
-        <div>
-            <h2 class="h3">Users</h2>
-            <p>Manage users here.</p>
-        </div>
-
-        <div>
-            <h2 class="h3">Events</h2>
-            <p>Manage events here.</p>
-        </div>
-
-        <div>
-            <h2 class="h3">Cities</h2>
-            <p>Manage cities here.</p>
-        </div>
-
-        <div>
-            <h2 class="h3">Places</h2>
-            <p>Manage places here.</p>
-        </div>
-
-        <div>
-            <h2 class="h3">Applications</h2>
-            <p>Manage applications here.</p>
+    <section class="container mt-5">
+        <div class="row g-4">
+            <div class="col-12 col-md-6 col-lg-2">
+                <h2 class="h3">Users</h2>
+                <p>Manage users here.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-2">
+                <h2 class="h3">Events</h2>
+                <p>Manage events here.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-2">
+                <h2 class="h3">Cities</h2>
+                <p>Manage cities here.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-2">
+                <h2 class="h3">Places</h2>
+                <p>Manage places here.</p>
+            </div>
+            <div class="col-12 col-md-6 col-lg-2">
+                <h2 class="h3">Applications</h2>
+                <p>Manage applications here.</p>
+            </div>
         </div>
     </section>
 </template>
