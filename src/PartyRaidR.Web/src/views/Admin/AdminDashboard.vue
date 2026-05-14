d3a55caa850a5045f175e51c099b0acefb9f5a1b
toth-mate <t0thm4t3@gmail.com>
2026-05-14 17:32:08 +0200

    feat: management cards are clickable and do the navigation

--- src/PartyRaidR.Web/src/views/Admin/AdminDashboard.vue
+++ src/PartyRaidR.Web/src/views/Admin/AdminDashboard.vue
@@ -3,31 +3,31 @@
 
     <section class="container mt-5">
         <div class="row g-4">
-            <div class="col-12 col-md-6 col-lg-2">
+            <div class="col-12 col-md-6 col-lg-2" v-on:click="navigateTo('/admin-dashboard/users')">
                 <div class="dashboard-card p-3 rounded h-100">
                     <h2 class="h3">Users</h2>
                     <p>Manage users here.</p>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-2">
+            <div class="col-12 col-md-6 col-lg-2" v-on:click="navigateTo('/admin-dashboard/events')">
                 <div class="dashboard-card p-3 rounded h-100">
                     <h2 class="h3">Events</h2>
                     <p>Manage events here.</p>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-2">
+            <div class="col-12 col-md-6 col-lg-2" v-on:click="navigateTo('/admin-dashboard/cities')">
                 <div class="dashboard-card p-3 rounded h-100">
                     <h2 class="h3">Cities</h2>
                     <p>Manage cities here.</p>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-2">
+            <div class="col-12 col-md-6 col-lg-2" v-on:click="navigateTo('/admin-dashboard/places')">
                 <div class="dashboard-card p-3 rounded h-100">
                     <h2 class="h3">Places</h2>
                     <p>Manage places here.</p>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-2">
+            <div class="col-12 col-md-6 col-lg-2" v-on:click="navigateTo('/admin-dashboard/applications')">
                 <div class="dashboard-card p-3 rounded h-100">
                     <h2 class="h3">Applications</h2>
                     <p>Manage applications here.</p>
@@ -36,6 +36,13 @@
         </div>
     </section>
 </template>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const navigateTo = (path) => router.push(path)
+</script>
 <style scoped>
 .dashboard-card { color: whitesmoke }
 .col-12:first-of-type .dashboard-card {
