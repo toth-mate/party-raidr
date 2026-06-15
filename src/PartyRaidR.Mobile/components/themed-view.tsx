3cbd92f56c3ff78561fb47e81cdf417aea561921
Tóth Máté <t0thm4t3@gmail.com>
2026-06-15 12:40:32 +0200

    feat: cleaned unnecessary assets and changed main view component definition

--- src/PartyRaidR.Mobile/components/themed-view.tsx
+++ src/PartyRaidR.Mobile/components/themed-view.tsx
@@ -1,14 +1,42 @@
 import { View, type ViewProps } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  safe?: boolean;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+export function ThemedView({ style, lightColor, darkColor, safe, ...otherProps }: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+  const insets = useSafeAreaInsets();
 
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  if(!safe) {
+    return (
+      <View
+        style={[
+          { backgroundColor },
+          style
+        ]}
+        {...otherProps}
+      />
+    );
+  }
+  return (
+    <View
+      style={[
+        { backgroundColor },
+        style,
+        {
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        },
+      ]}
+      {...otherProps}
+    />
+  );
 }
