741afc3edea789a04730595da9d7c56c7343ee2e
Tóth Máté <t0thm4t3@gmail.com>
2026-06-15 19:08:14 +0200

    feat: added Profile page

---
+++ src/PartyRaidR.Mobile/app/(tabs)/profile.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function ProfileScreen() {
+  return (
+    <>
+      <ThemedView safe={true}>
+        <ThemedText type="title">
+          Profile
+        </ThemedText>
+      </ThemedView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
--- src/PartyRaidR.Mobile/navigation/MainTabs.tsx
+++ src/PartyRaidR.Mobile/navigation/MainTabs.tsx
@@ -37,6 +37,13 @@ export default function MainTabs() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="plus.circle" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.circle" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
