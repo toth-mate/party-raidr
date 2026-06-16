ff78990976031679b02a7deaf04f4cf297f28572
Tóth Máté <t0thm4t3@gmail.com>
2026-06-16 19:09:12 +0200

    feat: fetching event list

--- src/PartyRaidR.Mobile/app/(tabs)/browse.tsx
+++ src/PartyRaidR.Mobile/app/(tabs)/browse.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import { eventService } from '@/services/eventService';
+
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { EventDisplayDto } from '@/types/event.types';
 
 export default function BrowseScreen() {
+  const [events, setEvents] = useState<EventDisplayDto[]>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    eventService.getAllDisplay()
+      .then((data) => setEvents(data));
+
+    setIsLoading(false);
+  }, []);
+
   return (
     <>
       <ThemedView safe={true}>
