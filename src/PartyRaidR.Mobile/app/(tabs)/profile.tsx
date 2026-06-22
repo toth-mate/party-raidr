dcef3a7d50630fdeff7abd10ffd59316dc814bc5
Tóth Máté <t0thm4t3@gmail.com>
2026-06-22 22:25:36 +0200

    feat: show username and email

--- src/PartyRaidR.Mobile/app/(tabs)/profile.tsx
+++ src/PartyRaidR.Mobile/app/(tabs)/profile.tsx
@@ -5,19 +5,29 @@ import { useAuthStore } from '@/store/useAuthStore';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import ThemedButton from '@/components/themed-button';
+import { useThemeColor } from '@/hooks/use-theme-color';
 
 export default function ProfileScreen() {
   const router = useRouter();
+  const user = useAuthStore(state => state.user);
   const isLoggedIn = useAuthStore(state => state.isAuthenticated);
   const logout = useAuthStore(state => state.logout);
+
+  const backgroundColor = useThemeColor({}, 'inputFieldBackground');
 
   if(isLoggedIn) {
     return (
       <>
         <ThemedView safe={true}>
-          <ThemedText type="title">
-            Profile
-          </ThemedText>
+          <ThemedView style={styles.header}>
+            <ThemedText type="title" style={styles.title}>
+              Welcome back!
+            </ThemedText>
+          </ThemedView>
+          <ThemedView style={[styles.content, { backgroundColor }]}>
+            <ThemedText style={styles.usernameText}>{user?.username}</ThemedText>
+            <ThemedText style={styles.emailText}>{user?.email}</ThemedText>
+          </ThemedView>
           <ThemedButton
             title="Logout"
             variant='danger'
@@ -53,5 +63,26 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     gap: 8,
+  },
+  header: {
+    borderBottomWidth: 0.8,
+    borderColor: '#999',
+    padding: 10,
+    marginBottom: 10,
+  },
+  title: {
+    fontWeight: '100',
+  },
+  content: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  usernameText: {
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+  emailText: {
+    fontSize: 14,
+    fontWeight: '300',
   },
 });
