8a4396a43391005800d664a911177993a1ccf73d
Tóth Máté <t0thm4t3@gmail.com>
2026-06-23 14:57:57 +0200

    docs: token method

--- src/PartyRaidR.Backend/Services/Promises/ITokenService.cs
+++ src/PartyRaidR.Backend/Services/Promises/ITokenService.cs
@@ -4,6 +4,12 @@ namespace PartyRaidR.Backend.Services.Promises
 {
     public interface ITokenService
     {
+        /// <summary>
+        /// Generates a JSON Web Token used for authentication.
+        /// The token holds the following information: Username, Email Address, Role (regular user/admin)
+        /// </summary>
+        /// <param name="user">The user whose information is to be contained by the token</param>
+        /// <returns>The token - a string value</returns>
         string GenerateToken(User user);
     }
 }
