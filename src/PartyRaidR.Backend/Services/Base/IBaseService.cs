3dcff81cdc66ec695a1c1c79edf53fa199c057ab
Tóth Máté <t0thm4t3@gmail.com>
2026-06-23 15:11:26 +0200

    refactor: base service methods

--- src/PartyRaidR.Backend/Services/Base/IBaseService.cs
+++ src/PartyRaidR.Backend/Services/Base/IBaseService.cs
@@ -14,11 +14,13 @@ namespace PartyRaidR.Backend.Services.Base
         /// <param name="id">The unique identifier of the entity.</param>
         /// <returns>A service response object containing the entity DTO.</returns>
         Task<ServiceResponse<TDto>> GetByIdAsync(string id);
+        
         /// <summary>
         /// Retieves all entities of the specified type.
         /// </summary>
         /// <returns>A service response object containing a collection of entity DTOs.</returns>
         Task<ServiceResponse<IEnumerable<TDto>>> GetAllAsync();
+
         /// <summary>
         /// Inserts a new entity into the database based on the provided DTO.
         /// The entity gets a unique identifier.
@@ -26,6 +28,7 @@ namespace PartyRaidR.Backend.Services.Base
         /// <param name="dto">The provided object in the form it is inserted in.</param>
         /// <returns>A service response containing the inserted entity object as a DTO.</returns>
         Task<ServiceResponse<TDto>> AddAsync(TDto dto);
+
         /// <summary>
         /// Updates an existing entity in the database based on the provided DTO object.
         /// The object must contain the unique ID, as that is used to find the entity in the database.
@@ -34,6 +37,7 @@ namespace PartyRaidR.Backend.Services.Base
         /// <param name="dto">The DTO object containing the updated data.</param>
         /// <returns>A service response containing the updated entity as a DTO object.</returns>
         Task<ServiceResponse<TDto>> UpdateAsync(TDto dto);
+
         /// <summary>
         /// Deletes an entity from the databse based on the provided ID.
         /// </summary>
