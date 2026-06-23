b611eb115685954bcd391bbee5bc648c4b85a83e
Tóth Máté <t0thm4t3@gmail.com>
2026-06-23 14:30:39 +0200

    docs: city method

--- src/PartyRaidR.Backend/Services/Promises/ICityService.cs
+++ src/PartyRaidR.Backend/Services/Promises/ICityService.cs
@@ -7,8 +7,25 @@ namespace PartyRaidR.Backend.Services.Promises
 {
     public interface ICityService : IBaseService<City, CityDto>
     {
+        /// <summary>
+        /// Get the number of places in a city.
+        /// </summary>
+        /// <param name="id">The unique identifier of the city</param>
+        /// <returns>An integer representing the number of places in the citiy</returns>
         Task<ServiceResponse<int>> GetNumberOfPlacesAsync(string id);
+
+        /// <summary>
+        /// Get a list of cities in a county.
+        /// </summary>
+        /// <param name="county">The county to look for cities in</param>
+        /// <returns>A list of cities in the county</returns>
         Task<ServiceResponse<IEnumerable<CityDto>>> GetByCountyAsync(string county);
+
+        /// <summary>
+        /// Get a list of trending cities.
+        /// "Trending" cities are the top five cities with the most places.
+        /// </summary>
+        /// <returns>A list of cities</returns>
         Task<ServiceResponse<List<CityDto>>> GetTrendingCitiesAsync();
     }
 }
