4dac2fa951377666a88ad0cef96c97939aaea333
Tóth Máté <t0thm4t3@gmail.com>
2026-07-17 10:21:16 +0200

    feat: show markers on map

--- src/PartyRaidR.Mobile/app/(tabs)/map.tsx
+++ src/PartyRaidR.Mobile/app/(tabs)/map.tsx
@@ -1,11 +1,24 @@
 import { StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 
 import { ThemedView } from '@/components/themed-view';
 import { ThemedText } from '@/components/themed-text';
+import { eventService } from '@/services/eventService';
+import { EventMarkerDto } from '@/types/event.types';
 
 const Map = () => {
+  const [events, setEvents] = useState<EventMarkerDto[]>([]);
+
+  useEffect(() => {
+    const fetchEvents = async() => {
+        const result = await eventService.getMarkerEvents();
+        setEvents(result);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <ThemedView style={styles.container}>
         <MapView
@@ -17,7 +30,16 @@ const Map = () => {
             }}
             style={styles.map}
         >
-            <Marker coordinate={{ latitude: 46.253, longitude: 20.148 }} title='Event name' description='Elm street 401.' />
+            {events.map((marker) => (
+                <Marker
+                    key={marker.id}
+                    coordinate={{
+                        latitude: marker.latitude,
+                        longitude: marker.longitude
+                    }}
+                    title={marker.title}
+                />
+            ))}
         </MapView>
     </ThemedView>
   )
