079504109c37792f42708597b035e335b68318a5
Tóth Máté <t0thm4t3@gmail.com>
2026-07-17 09:45:01 +0200

    feat: new DTO for marker data

---
+++ src/PartyRaidR.Shared/Dtos/EventMarkerDto.cs
@@ -0,0 +1,15 @@
+using PartyRaidR.Shared.Enums;
+using System.Text.Json.Serialization;
+
+namespace PartyRaidR.Shared.Dtos
+{
+    public class EventMarkerDto : IHasId
+    {
+        public string Id { get; set; } = Guid.Empty.ToString();
+        public string Title { get; set; } = string.Empty;
+        public DateTime StartingDate { get; set; } = DateTime.Now;
+        public DateTime EndingDate { get; set; } = DateTime.Now;
+        public double Latitude { get; set; } = 0f;
+        public double Longitude { get; set; } = 0f;
+    }
+}
