26222a03baeaca3b647f941baf18c6cab0032e56
Tóth Máté <t0thm4t3@gmail.com>
2026-07-17 09:28:47 +0200

    feat: inserted map component with initial coordinate

--- src/PartyRaidR.Mobile/app/(tabs)/map.tsx
+++ src/PartyRaidR.Mobile/app/(tabs)/map.tsx
@@ -1,17 +1,31 @@
 import { StyleSheet } from 'react-native';
 import React from 'react';
+import MapView from 'react-native-maps';
 
 import { ThemedView } from '@/components/themed-view';
 import { ThemedText } from '@/components/themed-text';
 
 const Map = () => {
   return (
-    <ThemedView safe>
-        <ThemedText>Map</ThemedText>
+    <ThemedView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <MapView
+            initialRegion={{
+                latitude: 46.253,
+                longitude: 20.148,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+            style={styles.map}
+/>
     </ThemedView>
   )
 }
 
 export default Map;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        width: '100%',
+        height: '100%',
+    }
+});
