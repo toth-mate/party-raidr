7fbd7c798130a6019326ac2f20b4a30d9654a712
Tóth Máté <t0thm4t3@gmail.com>
2026-07-17 17:01:16 +0200

    feat: fetch events and display on panel

--- src/PartyRaidR.Mobile/app/(tabs)/index.tsx
+++ src/PartyRaidR.Mobile/app/(tabs)/index.tsx
@@ -1,14 +1,41 @@
+import { StyleSheet, View } from 'react-native';
+
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { UpcomingEventDto } from '@/types/event.types';
+import { eventService } from '@/services/eventService';
+import { useThemeColor } from '@/hooks/use-theme-color';
 
 export default function HomeScreen() {
+  const [upcomingEvents, setUpcomingEvents] = useState<UpcomingEventDto[]>([]);
+  const panelBgColor = useThemeColor({}, 'inputFieldBackground');
+
+  useEffect(() => {
+    const fetchUpcomingEvents = async () => {
+      const result = await eventService.getUpcomingEvents();
+      setUpcomingEvents(result);
+    };
+
+    fetchUpcomingEvents();
+  }, []);
+
   return (
     <ThemedView safe>
       <ThemedView style={styles.container}>
         <ThemedText style={styles.title}>
           Welcome!
         </ThemedText>
+        <View style={[styles.upcomingEventsContainer, { backgroundColor: panelBgColor }]}>
+          <ThemedText type="subtitle">
+            Upcoming events:
+          </ThemedText>
+          {upcomingEvents.map((event) => (
+            <View key={event.id}>
+              <ThemedText>{event.title}</ThemedText>
+            </View>
+          ))}
+        </View>
       </ThemedView>
     </ThemedView>
   );
@@ -22,5 +49,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '200',
+  },
+  upcomingEventsContainer: {
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10,
   },
 });
