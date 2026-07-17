96ee346237131d10cf313864c8d9b45c3179b6e1
Tóth Máté <t0thm4t3@gmail.com>
2026-07-17 16:27:44 +0200

    feat: welcome message

--- src/PartyRaidR.Mobile/app/(tabs)/index.tsx
+++ src/PartyRaidR.Mobile/app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <>
-      <ThemedView safe={true}>
-        <ThemedText type="title">
-          Home
+    <ThemedView safe>
+      <ThemedView style={styles.container}>
+        <ThemedText style={styles.title}>
+          Welcome!
         </ThemedText>
       </ThemedView>
-    </>
+    </ThemedView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 5,
+    paddingVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '200',
+  },
+});
