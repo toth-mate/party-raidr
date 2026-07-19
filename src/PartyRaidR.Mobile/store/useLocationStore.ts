4cd0cc1d17400f8e80fc4c95e223f0b45f11be84
Tóth Máté <t0thm4t3@gmail.com>
2026-07-19 20:06:50 +0200

    feat: store location in global state

---
+++ src/PartyRaidR.Mobile/store/useLocationStore.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
+
+export const useLocationStore = create<LocationState>((set) => ({
+    lat: undefined,
+    lng: undefined,
+    loadLocation: async () => {
+        try {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            if(status !== 'granted') {
+                Alert.alert('Permission to access location was denied.', 'Please grant permission to access location for better experience.');
+                return;
+            }
+            const location = await Location.getCurrentPositionAsync({});
+            set({ lat: location.coords.latitude, lng: location.coords.longitude });
+        }
+        catch(error) {
+            console.error(`An error occured when loading location: ${error}`);
+        }
+    },
+}));
+
+interface LocationState {
+    lat: number | undefined;
+    lng: number | undefined;
+    loadLocation: () => Promise<void>;
+};
