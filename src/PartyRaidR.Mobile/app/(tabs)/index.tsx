bacb31e1da4cd9f249958723509deb7c34cdc672
Tóth Máté <t0thm4t3@gmail.com>
2026-07-19 16:42:49 +0200

    feat: community-section

--- src/PartyRaidR.Mobile/app/(tabs)/index.tsx
+++ src/PartyRaidR.Mobile/app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ import { eventService } from '@/services/eventService';
 import { useThemeColor } from '@/hooks/use-theme-color';
 import { Collapsible } from '@/components/ui/collapsible';
 import EventCard from '@/components/event-card';
+import ThemedButton from '@/components/themed-button';
 
 export default function HomeScreen() {
   const [upcomingEvents, setUpcomingEvents] = useState<UpcomingEventDto[]>([]);
@@ -28,16 +29,26 @@ export default function HomeScreen() {
         <ThemedText style={styles.title}>
           Welcome!
         </ThemedText>
+
         <View style={[styles.upcomingEventsContainer, { backgroundColor: panelBgColor }]}>
           <ThemedText type="subtitle" style={{ marginBottom: 5 }}>
             Upcoming events:
           </ThemedText>
+
           <Collapsible title="Show/Hide upcoming events" defaultOpen>
             {upcomingEvents.map((event) => (
               <EventCard key={event.id} event={event}/>
             ))}
           </Collapsible>
         </View>
+
+        <ThemedView>
+          <ThemedText type="title" centered>Join our community!</ThemedText>
+          <View style={styles.buttonContainer}>
+            <ThemedButton title="Register" onPress={() => null} style={styles.button} />
+            <ThemedButton title="Login" onPress={() => null} outline style={styles.button} />
+          </View>
+        </ThemedView>
       </ThemedView>
     </ThemedView>
   );
@@ -56,5 +67,14 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     marginTop: 10,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 8,
+  },
+  button: {
+    width: '45%',
   },
 });
