454328114320a5f90e0c7b92bb7451c363812d0a
Tóth Máté <t0thm4t3@gmail.com>
2026-07-24 14:41:54 +0200

    chore: ESLint config

--- src/PartyRaidR.Mobile/eslint.config.js
+++ src/PartyRaidR.Mobile/eslint.config.js
@@ -1,3 +1,5 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
@@ -7,4 +9,5 @@ module.exports = defineConfig([
   {
     ignores: ['dist/*'],
   },
+  eslintConfigPrettier,
 ]);
