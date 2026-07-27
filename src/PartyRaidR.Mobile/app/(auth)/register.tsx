a3e79c366825c6e8c7382fc0554095febbf7a777
Tóth Máté <t0thm4t3@gmail.com>
2026-07-27 20:55:09 +0200

    feat: register screen layout

--- src/PartyRaidR.Mobile/app/(auth)/register.tsx
+++ src/PartyRaidR.Mobile/app/(auth)/register.tsx
@@ -1,14 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import LabeledInput from '@/components/ui/labeled-input';
+
+const TRANSLATION_PREFIX = 'screens.auth.register.';
 
 const Register = () => {
+  const { t } = useTranslation();
+
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <ThemedView
+      safe
+      style={styles.container}>
+      <ThemedText
+        type='title'
+        centered>
+        {t(`${TRANSLATION_PREFIX}title`)}
+      </ThemedText>
+      <ThemedView style={styles.inputSection}>
+        <LabeledInput
+          labelKey={t(`${TRANSLATION_PREFIX}username`)}
+          placeholder={t(`${TRANSLATION_PREFIX}placeholders.usernameExample`)}
+        />
+      </ThemedView>
+      <ThemedView style={styles.inputSection}>
+        <LabeledInput
+          labelKey={t(`${TRANSLATION_PREFIX}email`)}
+          placeholder={t(`${TRANSLATION_PREFIX}placeholders.emailExample`)}
+        />
+      </ThemedView>
+      <ThemedView style={styles.inputSection}>
+        <LabeledInput
+          labelKey={t(`${TRANSLATION_PREFIX}password`)}
+          placeholder={t(`${TRANSLATION_PREFIX}placeholders.password`)}
+          secureTextEntry
+        />
+      </ThemedView>
+      <ThemedView style={styles.inputSection}>
+        <LabeledInput
+          labelKey={t(`${TRANSLATION_PREFIX}passwordConfirm`)}
+          placeholder={t(`${TRANSLATION_PREFIX}placeholders.passwordConfirm`)}
+          secureTextEntry
+        />
+      </ThemedView>
+      <ThemedView style={styles.inputSection}>
+        <LabeledInput
+          labelKey={t(`${TRANSLATION_PREFIX}dateOfBirth`)}
+          placeholder={t(`${TRANSLATION_PREFIX}placeholders.emailExample`)}
+          type='date'
+        />
+      </ThemedView>
+    </ThemedView>
   );
 };
 
 export default Register;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputSection: {
+    width: '95%',
+    marginTop: 15,
+  },
+});
