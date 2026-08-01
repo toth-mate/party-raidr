46871d79084163ab1f5c86df64617c4b6e9cf560
Tóth Máté <t0thm4t3@gmail.com>
2026-08-01 17:03:26 +0200

    feat: request object type

--- src/PartyRaidR.Mobile/types/auth.types.ts
+++ src/PartyRaidR.Mobile/types/auth.types.ts
@@ -13,4 +13,12 @@ export interface UserDto {
   role: UserRole;
 }
 
+export interface UserRegisterDto {
+  username: string;
+  email: string;
+  role: UserRole;
+  birthDate: string;
+  password: string;
+}
+
 export type UserRole = 'User' | 'Admin';
