3c520d1d5dbd413f68e9df15f2826896d76b4ccc
Tóth Máté <t0thm4t3@gmail.com>
2026-08-09 08:43:30 +0200

    feat: call hook after calculating visible part of the map

--- src/PartyRaidR.Mobile/app/(tabs)/map.tsx
+++ src/PartyRaidR.Mobile/app/(tabs)/map.tsx
@@ -1,39 +1,48 @@
 import { useRouter } from 'expo-router';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView, { Callout, Marker } from 'react-native-maps';
+import MapView, { Callout, Marker, Region } from 'react-native-maps';
 
 import MarkerContent from '@/components/marker-content';
 import { ThemedView } from '@/components/themed-view';
-import { eventService } from '@/services/eventService';
-import { EventMarkerDto } from '@/types/event.types';
+import { useMapEvents } from '@/hooks/use-event-queries';
+import { BoundingBox } from '@/types/event.types';
+
+const INITIAL_REGION: Region = {
+  latitude: 46.253,
+  longitude: 20.148,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
 
 const Map = () => {
+  const [bounds, setBounds] = useState<BoundingBox | null>(null);
+  const { data: events } = useMapEvents(bounds);
   const router = useRouter();
-  const [events, setEvents] = useState<EventMarkerDto[]>([]);
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      const result = await eventService.getMarkerEvents();
-      setEvents(result);
-    };
+  const handleRegionChange = (newRegion: Region) => {
+    if (newRegion.latitudeDelta > 0.5) {
+      return;
+    }
 
-    fetchEvents();
-  }, []);
+    // Calculate the bounding box
+    const minLat = newRegion.latitude - newRegion.latitudeDelta / 2,
+      maxLat = newRegion.latitude + newRegion.latitudeDelta / 2,
+      minLng = newRegion.longitude - newRegion.longitudeDelta / 2,
+      maxLng = newRegion.longitude + newRegion.longitudeDelta / 2;
+
+    setBounds({ minLat, maxLat, minLng, maxLng });
+  };
 
   return (
     <ThemedView style={styles.container}>
       <MapView
-        initialRegion={{
-          latitude: 46.253,
-          longitude: 20.148,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        initialRegion={INITIAL_REGION}
+        onRegionChangeComplete={handleRegionChange}
         rotateEnabled={false}
         showsPointsOfInterest={false}
         style={styles.map}>
-        {events.map(marker => (
+        {events?.map(marker => (
           <Marker
             key={marker.id}
             coordinate={{
--- src/PartyRaidR.Mobile/hooks/use-event-queries.ts
+++ src/PartyRaidR.Mobile/hooks/use-event-queries.ts
@@ -2,10 +2,10 @@ import { eventService } from '@/services/eventService';
 import { BoundingBox } from '@/types/event.types';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
-export const useMapEvents = (bounds: BoundingBox) => {
+export const useMapEvents = (bounds: BoundingBox | null) => {
   return useQuery({
     queryKey: ['events', 'map', bounds],
-    queryFn: () => eventService.getMarkerEvents(bounds),
+    queryFn: () => eventService.getMarkerEvents(bounds!),
     enabled: !!bounds,
     placeholderData: keepPreviousData
   });
