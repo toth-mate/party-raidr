f9a35e603432f4746f389fbe5d52694eaec01b71
Tóth Máté <t0thm4t3@gmail.com>
2026-08-14 16:30:35 +0200

    feat: applying API call

--- src/PartyRaidR.Mobile/services/applicationService.ts
+++ src/PartyRaidR.Mobile/services/applicationService.ts
@@ -1,6 +1,10 @@
 import { apiClient } from '@/api/apiClient';
+import { ApplicationDto } from '@/types/application.types';
 
 export const applicationService = {
+  apply: async (application: ApplicationDto): Promise<void> => {
+    await apiClient.post('/application', application);
+  },
   exists: async (id: string): Promise<boolean> => {
     const response = await apiClient.get<boolean>('/application/exists', {
       params: id,
---
+++ src/PartyRaidR.Mobile/types/application.types.ts
@@ -0,0 +1,7 @@
+export interface ApplicationDto {
+  id: string;
+  userId: string;
+  eventId: string;
+  timeOfApplication: string;
+  status: number;
+}
