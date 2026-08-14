a3cf55083ebf06714b2d7c582d091713f0811c2c
Tóth Máté <t0thm4t3@gmail.com>
2026-08-14 16:39:20 +0200

    feat: mutation hook

---
+++ src/PartyRaidR.Mobile/hooks/use-apply.ts
@@ -0,0 +1,16 @@
+import { applicationService } from '@/services/applicationService';
+import { ApplicationDto } from '@/types/application.types';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export const useApply = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (application: ApplicationDto) => applicationService.apply(application),
+    onSuccess: (_, application) => {
+      queryClient.invalidateQueries({
+        queryKey: ['application', 'exists', { eventId: application.eventId }],
+      });
+    },
+  });
+};
