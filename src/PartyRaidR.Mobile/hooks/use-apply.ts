3b6590afac553511428da90c27443beb7f68de85
Tóth Máté <t0thm4t3@gmail.com>
2026-08-16 19:02:10 +0200

    feat: show toast on success and error

--- src/PartyRaidR.Mobile/hooks/use-apply.ts
+++ src/PartyRaidR.Mobile/hooks/use-apply.ts
@@ -1,9 +1,12 @@
 import { applicationService } from '@/services/applicationService';
 import { ApplicationDto } from '@/types/application.types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
+import Toast from 'react-native-toast-message';
 
 export const useApply = () => {
   const queryClient = useQueryClient();
+  const { t } = useTranslation();
 
   return useMutation({
     mutationFn: (application: ApplicationDto) =>
@@ -12,6 +15,17 @@ export const useApply = () => {
       queryClient.invalidateQueries({
         queryKey: ['application', 'exists'],
       });
+
+      Toast.show({
+        text1: t('screens.event.toast.success.title'),
+        text2: t('screens.event.toast.success.text'),
+      });
     },
+    onError: () =>
+      Toast.show({
+        type: 'error',
+        text1: t('screens.event.toast.error.title'),
+        text2: t('screens.event.toast.error.text'),
+      }),
   });
 };
