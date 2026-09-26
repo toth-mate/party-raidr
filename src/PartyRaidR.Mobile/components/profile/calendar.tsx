7f8ab7bbf802411d267abd8c675dafdd4189cbe3
Máté Tóth <t0thm4t3@gmail.com>
2026-09-26 08:28:32 +0200

    feat: calendar component inserted to profile screen

---
+++ src/PartyRaidR.Mobile/components/profile/calendar.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { ThemedText } from '../themed-text';
+import { ThemedView } from '../themed-view';
+
+const Calendar = () => {
+  return (
+    <ThemedView>
+      <ThemedText>Calendar</ThemedText>
+    </ThemedView>
+  );
+};
+
+export default Calendar;
+
+const styles = StyleSheet.create({});
